dc4a67651ba5511ac10c6cc494c3718786b950e4
Mai <mai.zakaria.18@gmail.com>
2023-11-02 05:15:34 -0700

    python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+if __name__ != "__main__":
+import sys
+arg_n = len(sys.argv) - 1
+if arg_n = 0:
+    print("{} arguments".format(arg_n))
+elif arg_n = 1:
+    print("{} argument".format(arg_n))
+elif arg_n > 1:
+    print("{} arguments".format(arg_n))
+for i in range(arg_n):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
