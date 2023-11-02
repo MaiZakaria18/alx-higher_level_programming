cef45d495df1f97aa73ed9617a83aead99de2383
Mai <mai.zakaria.18@gmail.com>
2023-11-02 07:06:23 -0700

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     op = sys.argv[2]
-    if op != "+" and  op != "-" and op != "*" and op != "/":
+    if op != "+" and op != "-" and op != "*" and op != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     a = int(sys.argv[1])
