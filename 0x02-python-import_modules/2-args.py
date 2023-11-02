b72b9383bc08fb129d73e252db9fcb3dbb502996
Mai <mai.zakaria.18@gmail.com>
2023-11-02 05:56:57 -0700

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
 
     arg_n = len(sys.argv) - 1
     if arg_n == 0:
-        print("{} arguments".format(arg_n))
+        print("0 arguments")
     elif arg_n == 1:
-        print("{} argument".format(arg_n))
+        print("1 argument")
     elif arg_n > 1:
         print("{} arguments".format(arg_n))
     for i in range(arg_n):
