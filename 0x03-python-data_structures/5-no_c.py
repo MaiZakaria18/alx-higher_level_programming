57bda5bfda89e33c8c7ef2347438dec44dac1d56
Mai <mai.zakaria.18@gmail.com>
2023-11-05 09:24:19 -0800

    data-structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    copy =[x for x in my_string if x != 'c' and 'C']
-    return("".join(copy))
+    copy = [x for x in my_string if x != 'c' and 'C']
+    return ("".join(copy))
