193828a25aca4077afb4d4357290d27a7051cbe9
Mai <mai.zakaria.18@gmail.com>
2023-11-05 08:50:28 -0800

    data-structures

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx >= len(my_list) or idx < 0:
+        return my_list
+    copy_list = my_list.copy()
+    copy_list[idx] = element
+    return copy_list
