f4e678e066aecc66c12be2924260264b2697f4df
Mai <mai.zakaria.18@gmail.com>
2023-11-13 06:46:22 -0800

    data-structure

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dic = {}
     for keys in a_dictionary:
-        new_dic = a_dictionary[keys] * 2
+        new_dic[keys] = a_dictionary[keys] * 2
     return new_dic
