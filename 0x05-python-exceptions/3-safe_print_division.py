87a6bf59b822d83237cf94f9a9ec7bdcf0bb059e
Mai <mai.zakaria.18@gmail.com>
2023-11-20 07:44:49 -0800

    p

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
 
     try:
         res = a / b
-    except ValueError:
+    except ZeroDivisionError:
         res = None
     finally:
         print("Inside result: {}".format(res))
