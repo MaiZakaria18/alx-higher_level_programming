f65074066a633e4d894df2e2f9453ef4f7cc7353
Your Name <you@example.com>
2023-11-22 18:54:39 +0200

    oop

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,3 +15,6 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
