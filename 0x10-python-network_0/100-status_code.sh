7a4d3e7e163f1ae32c9bb4d3b94c4f7ddf047d3e
Mai <mai.zakaria.18@gmail.com>
2024-02-29 14:00:51 -0800

    task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 # script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -L -X HEAD -w "%{http_code}" "$1"
+awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
